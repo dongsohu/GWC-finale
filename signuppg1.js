36f0c30ab5af35b81f25e3ce60aab7be5d6d9ff6
Danielle Ongsohu <ongsohu.d@gmail.com>
2018-08-09 14:12:06 +0000

    Draft 5

--- signuppg1.js
+++ signuppg1.js
@@ -1,6 +1,26 @@
-
-var nameInput = '';
+const nameInput = '';
 var emaiInput = '';
 var passwordInput = '';
+// above are empty variables whatever the user types will be put into the variable 
+// 
+var usersObject = {
+      
 
-if 
+};
+/*global localStorage*/
+if (typeof(Storage) !== "undefined") {
+    // Store
+    localStorage.setItem("lastname", "Smith");
+    // Retrieve
+    document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+} else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+}
+
+/*global placeholder*/
+if (placeholder === null ){
+    alert("You must fill out all every feild.");
+    
+}else {
+    
+}
