793e6048d91f8dc5ce022602e8be80cdf586dec4
Danielle Ongsohu <ongsohu.d@gmail.com>
2018-08-10 19:49:28 +0000

    Draft8

--- signuppg1.js
+++ signuppg1.js
@@ -1,10 +1,13 @@
 var nameInput = document.getelementbyID("usernameInsert").value;
-var emaiInput = document.getelementbyID("emailInsert").value;
+var emailInput = document.getelementbyID("emailInsert").value;
 var passwordInput = document.getelementID("usersPswrd").value;
+var passwordInput2 = document.getelementID("usersPswrd2").value;
 // above are empty variables whatever the user types will be put into the variable 
 var usersObject = {
-      
-
+        password :"" ,
+        email: "",
+        grades : "",
+        priorityList : ""
 };
 /*global localStorage*/
 if (typeof(Storage) !== "undefined") {
@@ -20,11 +23,38 @@ if (typeof(Storage) !== "undefined") {
 if (placeholder === null ){
     alert("You must fill out all every feild.");
     
-}else (){
+}else{
     
 }
+var newUser= new usersObject()
+newUser.password = passwordInput
+newUser.email = emailInput
+function signupFunct(newUser, userInput) {
+    localStorage.setItem(newUser,userInput);
+}
+function filledOut(userName, userEmail, userPswrd, userPswrd2){
+        var a = document.forms["signUp"]["createUserName"].value
+        var b = document.forms["signUp"]["createEmail"].value
+        var c = document.forms["signUp"]["createPswrd"].value
+        var d = document.forms["signUp"]["createPswrd2"].value
+        var allFilled = false
+        if (a == ""){
+            allFilled = true;
+        }
+        if (b == ""){
+            allFilled = true;
+        }
+        if (c == ""){
+            allFilled = true;
+        }
+        if (d == ""){
+            allFilled = true;
+        }
+        if (c == d){
+            allFilled = true;
+        }
+        
+        return allFilled;
+}
+var signUpResult = allFilled(userName, userEmail, userPswrd, userPswrd2)
 
-function signupFunct {
-    
-    
-}
