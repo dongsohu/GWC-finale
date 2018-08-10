86bbe315d62448ed680c3501bd8273fba20673ee
Danielle Ongsohu <ongsohu.d@gmail.com>
2018-08-10 15:15:29 +0000

    Draft 7

--- signuppg1.js
+++ signuppg1.js
@@ -9,9 +9,9 @@ var usersObject = {
 /*global localStorage*/
 if (typeof(Storage) !== "undefined") {
     // Store
-    localStorage.setItem("user", "Smith");
+    localStorage.setItem("username", "");
     // Retrieve
-    document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+    document.getElementById("result").innerHTML = localStorage.getItem("username");
 } else {
     document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
 }
@@ -20,7 +20,7 @@ if (typeof(Storage) !== "undefined") {
 if (placeholder === null ){
     alert("You must fill out all every feild.");
     
-}else {
+}else (){
     
 }
 
