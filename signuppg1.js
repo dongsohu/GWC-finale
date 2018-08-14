086ba2a0ad21234086928cdb7c47687cab4ba1ed
Danielle Ongsohu <ongsohu.d@gmail.com>
2018-08-14 15:56:58 +0000

    Draft 10

--- signuppg1.js
+++ signuppg1.js
@@ -1,7 +1,7 @@
-var nameInput = document.getelementbyID("usernameInsert").value;
-var emailInput = document.getelementbyID("emailInsert").value;
-var passwordInput = document.getelementID("usersPswrd").value;
-var passwordInput2 = document.getelementID("usersPswrd2").value;
+var nameInput = document.getElementById("usernameInsert").value;
+var emailInput = document.getElementById("emailInsert").value;
+var passwordInput = document.getElementById("usersPswrd").value;
+var passwordInput2 = document.getElementById("usersPswrd2").value;
 // above are empty variables whatever the user types will be put into the variable 
 var usersObject = {
     password :"" ,
@@ -11,6 +11,7 @@ var usersObject = {
 };
 
 /*global localStorage*/
+/*
 if (typeof(Storage) !== "undefined") {
     // Store
     localStorage.setItem("username", "");
@@ -19,22 +20,18 @@ if (typeof(Storage) !== "undefined") {
 } else {
     document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
 }
+*/
 
-/*global placeholder*/
-if (placeholder === null ){
-    alert("You must fill out all every field.");
-    
-}else{
-    
-}
-var newUser= new usersObject()
+
+var newUser= new usersObject();
 newUser.password = passwordInput;
 newUser.email = emailInput;
 function signupFunct(newUse, userInput) {
-    localStorage.setItem(newUse,userInput);
+    localStorage.setItem("test",userInput);
 }
-
-/*function filledOut(userName, userEmail, userPswrd, userPswrd2){
+signupFunct(nameInput,newUser);
+/*
+function filledOut(userName, userEmail, userPswrd, userPswrd2){
         var a = document.forms["signUp"]["createUserName"].value
         var b = document.forms["signUp"]["createEmail"].value
         var c = document.forms["signUp"]["createPswrd"].value
@@ -58,30 +55,62 @@ function signupFunct(newUse, userInput) {
         
         return allFilled;
 }
-*/
+
 var signUpResult = filledOut(nameInput, emailInput, passwordInput, passwordInput2);
 /*global*/
+/*
 if (input != null){
 $("document").ready(function(){
     $("#signupsNextbutton").click(function(){
-        e.preventDefault();
+    e.preventDefault();
         $('#dictionary').load('sign2.html');
         
     });
+*/    
 
-/*
 //login functions//
 var e = document.forms["loginForm"]["nameEmail"].value
 var f = document.forms["loginForm"]["logPSWRD"].value
+
 function logIN(UserName, UserPswrd){
+    
     if (localStorage.getItem(UserName)== null){
        alert("No username found!");
-   } if(localStorage.getItem(UserName).password  == UserPswrd){
-        $("document").ready(function(){
-            $("#enterBut").click(function(){
-                e.preventDefault();
-                $('#dictionary').load('basicpg.html');
-   }else{
+       $("#enterBut").prop('disabled', true);
+   }    
+    if(localStorage.getItem(UserName).password  == UserPswrd){
+      }  
+     else if(localStorage.getItem(UserName).password  != UserPswrd){
     alert("Wrong Password or Username.");
+    $("#enterBut").prop('disabled', true);
    }
-*/
+}
+
+function checkInput(){
+    var isValid = true;
+    /*global $*/
+    $('input').filter("[required]").each(function(){
+        if($(this).val() === ''){
+            $("#next1").prop('disabled', true);
+            isValid = false;
+            return false;
+        }
+        else{
+             $("#next1").prop('disabled', false);
+        }
+    });
+    if(isValid) {$("#next1").prop('disabled', false)}
+    return isValid;
+}
+
+$("#next1").click(function(){
+    alert(checkInput());
+});
+
+
+$("input").filter('[required]').on('keyup',function(){
+  checkInput();  
+});
+checkInput();
+
+alert(localStorage.getItem("dong"));
